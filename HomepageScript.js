97e0eb6caaba55b5817576a25239304303f82433
tek-wizard <apdprateeksingh@gmail.com>
2024-12-30 04:24:31 +0530

    Crads And  Dark-mode Added

--- HomepageScript.js
+++ HomepageScript.js
@@ -31,3 +31,15 @@ window.addEventListener("click" , function(e){
 
  
 })
+
+document.querySelector(".day-night input").addEventListener("change", () => {
+    document.querySelector("body").classList.add("toggle");
+    setTimeout(() => {
+        document.querySelector("body").classList.toggle("light");
+
+        setTimeout(
+            () => document.querySelector("body").classList.remove("toggle"),
+            10
+        );
+    }, 5);
+});
